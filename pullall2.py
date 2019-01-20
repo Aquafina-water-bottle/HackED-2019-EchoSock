3d5e9d77f028afb0ddd8b2870b50ae5e4df69783
Austin Siew <glowing.locker@gmail.com>
2019-01-20 10:39:42 -0700

    use pullall2.py

--- pullall2.py
+++ pullall2.py
@@ -1,5 +1,7 @@
 import os
 import time
+import logging
+import argparse
 import datetime
 from typing import NamedTuple
 
@@ -17,94 +19,71 @@ devices = set()
 
 # esp_id: number
 boards = {
-    "A": "?",
     "C": "?",
-    "H": "?"
+    "H": "?",
+    "A": "?",
     }
-
-# (esp_id, cycle_id) : last time they were used
-cycle_ids = {}
-
-class Device(NamedTuple):
-    # esp board ID: uppercase A-Z
-    esp_id: str
-
-    # cycle id: uppercase A-Z
-    cycle_id: str
-
-    # 4 chars
-    mac_address: str
 
 class Board(NamedTuple):
     # esp board ID: uppercase A-Z
     esp_id: str
     number: int
+    recorded_time: int
 
-def decode_message(message: str):
-    esp_id = message[0]
-    cycle_id = message[1]
-    addresses_str = message[2:]
-    for i in range(0, len(addresses_str), 4):
-        address = addresses_str[i:i+4]
-        if address == "0000":
-            break
 
-        #pylint: disable=too-many-function-args
-        device = Device(esp_id, cycle_id, address)
-        devices.add(device)
-        #print("esp id:", esp_id, "cycle id:", cycle_id, "addr:", addresses_str[i:i+4])
-
-        cycle_ids[(esp_id, cycle_id)] = time.time()
-
+def reset_messages():
+    for key in boards:
+        if time.time() - boards[key].recorded_time > 10:
+            logging.info("reset " + key)
+            boards[key] = "?"
 
 def decode_message2(message: str):
     """
     {esp_id}{#}
     """
+    #try:
+    esp_id = message[0]
+    number = int(message[1:])
+    boards[esp_id] = Board(esp_id, number, time.time())
+
     try:
-        esp_id = message[0]
-        number = int(message[1:])
-        boards[esp_id] = number
-        print(datetime.datetime.now(), "%3s %3s %3s" % tuple(boards[c] for c in 'CHA'))
+        formatted_strs = []
+        for key in boards:
+            if boards[key] == "?":
+                formatted_strs.append(" ? ")
+            else:
+                formatted_strs.append("%3s" % boards[key].number)
+
+        print(datetime.datetime.now(), ":",  " ".join(formatted_strs))
+        #print(datetime.datetime.now(), " ".join(("%3s" % boards[c].number) for c in 'CHA'))
+
+        logging.info("success: " + message)
+
+        # checks if any has to be reset to "?"
+        reset_messages()
+
     except Exception:
-        print("idk", repr(message))
-
-def check_ids():
-    for key in cycle_ids:
-        if time.time() - cycle_ids[key] > 10:
-            print(key)
-            for device in devices:
-                if (device.esp_id, device.cycle_id) == key:
-                    print(device)
-                devices.remove(device)
-            cycle_ids.pop(key)
+        logging.info("error: " + message)
 
 
 
-def main():
-    subscriber = pubsub_v1.SubscriberClient()
-    # The `subscription_path` method creates a fully qualified identifier
-    # in the form `projects/{project_id}/subscriptions/{subscription_name}`
-    subscription_path = subscriber.subscription_path( # pylint: disable=no-member
-        project_id, subscription_name)
+def parse_args():
+    # Usage: main.py fileName [output_path] [-d, --debug]
+    parser = argparse.ArgumentParser()
 
-    def callback(message):
-        message = message.data.decode("utf-8")
-        decode_message(message)
-        #print(datetime.datetime.now(), message.data.decode("utf-8"))
-        message.ack()
-        check_ids()
+    # debug
+    parser.add_argument("-d", "--debug", action="store_true")
 
-    subscriber.subscribe(subscription_path, callback=callback)
+    args = parser.parse_args()
+    if args.debug:
+        logger = logging.getLogger()
+        logger.setLevel("INFO")
 
-    # The subscriber is non-blocking. We must keep the main thread from
-    # exiting to allow it to process messages asynchronously in the background.
-    print('Listening for messages on {}'.format(subscription_path))
 
-    while True:
-        time.sleep(10)
 
 def main2():
+    parse_args()
+
     subscriber = pubsub_v1.SubscriberClient()
     # The `subscription_path` method creates a fully qualified identifier
     # in the form `projects/{project_id}/subscriptions/{subscription_name}`
@@ -124,13 +103,9 @@ def main2():
     print('Listening for messages on {}'.format(subscription_path))
 
     while True:
-        time.sleep(10)
+        time.sleep(5)
+        reset_messages()
 
-
-def test():
-    decode_message("AB12343456abcd0000lolo")
-    print(devices)
-    print(cycle_ids)
 
 def test2():
     decode_message2("A24")
@@ -141,8 +116,6 @@ def test2():
 
 
 if __name__ == "__main__":
-    #test2()
-    #main()
     main2()
 
 
