71db28d55cb7bad96caddbdeef205c12955646fa
Austin Siew <glowing.locker@gmail.com>
2019-01-20 09:23:12 -0700

    python works \o/

---
+++ pullall.py
@@ -0,0 +1,144 @@
+import os
+import time
+import datetime
+from typing import NamedTuple
+
+from google.cloud import pubsub_v1
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/austin/pgc/prog/Events/HackED-Jan-2019/temp/auth.json"
+
+project_id = "steady-mason-229223"
+subscription_name = "iot-sample-sub"
+
+
+
+# set of Device
+devices = set()
+
+# esp_id: number
+boards = {
+    "A": "?",
+    "B": "?",
+    "C": "?"
+    }
+
+# (esp_id, cycle_id) : last time they were used
+cycle_ids = {}
+
+class Device(NamedTuple):
+    # esp board ID: uppercase A-Z
+    esp_id: str
+
+    # cycle id: uppercase A-Z
+    cycle_id: str
+
+    # 4 chars
+    mac_address: str
+
+class Board(NamedTuple):
+    # esp board ID: uppercase A-Z
+    esp_id: str
+    number: int
+
+def decode_message(message: str):
+    esp_id = message[0]
+    cycle_id = message[1]
+    addresses_str = message[2:]
+    for i in range(0, len(addresses_str), 4):
+        address = addresses_str[i:i+4]
+        if address == "0000":
+            break
+
+        #pylint: disable=too-many-function-args
+        device = Device(esp_id, cycle_id, address)
+        devices.add(device)
+        #print("esp id:", esp_id, "cycle id:", cycle_id, "addr:", addresses_str[i:i+4])
+
+        cycle_ids[(esp_id, cycle_id)] = time.time()
+
+
+def decode_message2(message: str):
+    """
+    {esp_id}{#}
+    """
+    esp_id = message[0]
+    number = int(message[1:])
+    boards[esp_id] = number
+    print("%3s %3s %3s" % tuple(boards[c] for c in 'ABC'))
+
+def check_ids():
+    for key in cycle_ids:
+        if time.time() - cycle_ids[key] > 10:
+            print(key)
+            for device in devices:
+                if (device.esp_id, device.cycle_id) == key:
+                    print(device)
+                devices.remove(device)
+            cycle_ids.pop(key)
+
+
+
+def main():
+    subscriber = pubsub_v1.SubscriberClient()
+    # The `subscription_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/subscriptions/{subscription_name}`
+    subscription_path = subscriber.subscription_path( # pylint: disable=no-member
+        project_id, subscription_name)
+
+    def callback(message):
+        message = message.data.decode("utf-8")
+        decode_message(message)
+        #print(datetime.datetime.now(), message.data.decode("utf-8"))
+        message.ack()
+        check_ids()
+
+    subscriber.subscribe(subscription_path, callback=callback)
+
+    # The subscriber is non-blocking. We must keep the main thread from
+    # exiting to allow it to process messages asynchronously in the background.
+    print('Listening for messages on {}'.format(subscription_path))
+
+    while True:
+        time.sleep(10)
+
+def main2():
+    subscriber = pubsub_v1.SubscriberClient()
+    # The `subscription_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/subscriptions/{subscription_name}`
+    subscription_path = subscriber.subscription_path( # pylint: disable=no-member
+        project_id, subscription_name)
+
+    def callback(message):
+        data = message.data.decode("utf-8")
+        decode_message2(data)
+        #print(datetime.datetime.now(), message.data.decode("utf-8"))
+        message.ack()
+
+    subscriber.subscribe(subscription_path, callback=callback)
+
+    # The subscriber is non-blocking. We must keep the main thread from
+    # exiting to allow it to process messages asynchronously in the background.
+    print('Listening for messages on {}'.format(subscription_path))
+
+    while True:
+        time.sleep(10)
+
+
+def test():
+    decode_message("AB12343456abcd0000lolo")
+    print(devices)
+    print(cycle_ids)
+
+def test2():
+    decode_message2("A24")
+    decode_message2("B23")
+    decode_message2("C22")
+    decode_message2("A21")
+    decode_message2("C20")
+
+
+if __name__ == "__main__":
+    #test2()
+    #main()
+    main2()
+
--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Component } from 'react';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
-var Circle = React.createClass({
-    render: function() {
-        return (
-            <View style={styles.circle} />
-        )
-    }
-});
-
-
-circle: {
-    width: 100,
-    height: 100,
-    borderRadius: 100/2,
-    backgroundColor: 'red'
+class App extends Component {
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Some text on canvas" fontSize={15} />
+          <Rect
+            x={20}
+            y={50}
+            width={100}
+            height={100}
+            fill="red"
+            shadowBlur={10}
+          />
+          <Circle x={200} y={100} radius={50} fill="green" />
+          <Line
+            x={20}
+            y={200}
+            points={[0, 0, 100, 0, 100, 100]}
+            tension={0.5}
+            closed
+            stroke="black"
+            fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+            fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+            fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+          />
+        </Layer>
+      </Stage>
+    );
+  }
 }
+
+render(<App />, document.getElementById('root'));
 
